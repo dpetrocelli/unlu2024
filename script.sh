ef6edc2baac4f96f3179b7fac880305a0b1d0f1e
DP <dmpetrocelli@gmail.com>
2024-03-05 20:44:34 -0300

    x

---
+++ script.sh
@@ -0,0 +1,51 @@
+192.168.0.1 8080 WS1 
+192.168.0.2 8080 WS2 
+
+TCP / IP 
+-> WS1 a WS2 
+
+TCP SNC -> 
+192.168.0.1:8080 Tomcat 
+192.168.0.1:8081 Springboot
+
+curl / wget / 
+M1 a M" -> 192.168.0.1:8080 Tomc.
+
+------------------------ PC LOCAL / 
+Servidor de juegos : 127.0.0.1:8080 RMI 
+=
+cliente: pegar a server (X 127.0.0.1:8080)
+MONOLÍTICO
+------------------------
+L5 http / MQTT / ---- 
+----
+L4 tcp - Sockets (UDP / TCP)
+L3 ip
+
+API / WEB SERVER 
+
+                 BACKEND
+                 BACKEND
+FRONT END <----> BACKEND <----> BASE DE DATOS 
+                 BACKEND
+                 BACKEND
+mICROSERVICIOS 
+
+FRONT
+\
+V 
+BACKE
+\
+V
+BD
+\
+V
+x
+....
+
+
+Transparencia
+Esc
+Dispo
+TF 
+Het
